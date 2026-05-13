c589a36ddb7ec7c87e5aa3036ca419b33c81cf19
gottlied <mdivyabharathi830@gmail.com>
2026-05-13 16:01:57 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "divyabharatidevops" # Replace with your actual S3 bucket name
+    bucket = "divya07052026" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
